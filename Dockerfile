dc992dca2cc8bec1f7090b9dfb08b7336b372e6a
Matthias Vogel <m.vogel@andersundsehr.com>
2025-05-08 16:20:29 +0200

    🐛 fix version number in dockerfile

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-#FROM webdevops/php-nginx:8.3-alpine
-FROM pluswerk/php-dev:nginx-8.3-alpine
-
-COPY . /app/
-WORKDIR /app
-ENV WEB_DOCUMENT_ROOT=/app/public \
-    RAH_STORAGE_PATH=/storage
-
-VOLUME /storage
--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
+set -x
+
+LAST_GIT_TAG=$(git tag --points-at HEAD)
 
 docker build \
  -t andersundsehr/rah:latest \
- --build-arg LAST_GIT_TAG=$(git tag --points-at HEAD) \
+ --build-arg LAST_GIT_TAG=$LAST_GIT_TAG \
  -f build/Dockerfile \
  .
